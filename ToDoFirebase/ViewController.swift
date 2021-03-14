c348021244351bc3276d3366505ed4a9c8c2914e
onurbasdas <onurebasdase@hotmail.com>
2021-03-14 15:13:24 +0300

    login screen completed

--- ToDoFirebase/ViewController.swift
+++ ToDoFirebase/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func registerClicked(_ sender: Any) {
+        if emailText.text != nil && passwordText.text != nil{
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (result, error) in
+                if error != nil{
+                    print(error?.localizedDescription as Any)
+                }else{
+                    let uid = result?.user.uid
+                    let ref = Database.database().reference(withPath: "Users").child(uid!)
+                    ref.setValue(["email" : self.emailText.text!, "password" : self.passwordText.text!])
+                }
+            }
+        }
+    }
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        if emailText.text != nil && passwordText.text != nil{
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (result, error) in
+                if error != nil{
+                    print(error?.localizedDescription as Any)
+                }else{
+//                    let uid = result?.user.uid
+                }
+            }
+        }
+    }
 }
 
